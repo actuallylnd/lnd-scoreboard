f34b71e743dc6a165ef6321a8e8db541c3f9b324
lnd <99372239+actuallylnd@users.noreply.github.com>
2024-11-20 01:09:53 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'lnd'
+lua54 'yes'
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
+
+
+shared_scripts {
+    '@ox_lib/init.lua'
+}

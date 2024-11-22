1a0a2e072bfea812ee3a647bd0de93acb22b0a7d
lnd <99372239+actuallylnd@users.noreply.github.com>
2024-11-22 13:27:04 +0100

    Update server.lua

--- server.lua
+++ server.lua
@@ -3,6 +3,9 @@ local playersCheckingID = {}
 RegisterNetEvent("lnd-scoreboard:checkID")
 AddEventHandler("lnd-scoreboard:checkID", function(isChecking)
     local src = source
+    local xPlayer = ESX.GetPlayerFromId(src)
+    local group = xPlayer.getGroup()
+
     if isChecking then
         playersCheckingID[src] = true
     else
@@ -10,4 +13,5 @@ AddEventHandler("lnd-scoreboard:checkID", function(isChecking)
     end
 
     TriggerClientEvent("lnd-scoreboard:updateCheckers", -1, playersCheckingID)
+    TriggerClientEvent("lnd-scoreboard:updateGroup", src, group)
 end)
